e97aa4718364ece7ea1183a349a1c33b64e62524
Moisem <al221910022@gmail.com>
2020-10-12 17:56:25 -0500

    Metodos all y find consultas en BD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\alumnos; 
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,4 +24,22 @@ Route::get('Miprimerlayouts', function(){
 Route::get('vista', function(){ 
     return view('vista'); 
 });
-Route::get('alumno', 'alumnos@index');
+Route::get('carrera', 'alumnos@index');
+
+
+Route::get('alumnos', 'controllerjson@index');
+
+Route::get('alumno/{id}', 'controllerjson@store');
+
+
+//Route::get('/miprimerarray', 'controllerjson@getAlumnos')->name('alumnos');
+/*Route::get('alumnos3', function(){
+    $alumno = new alumnos;
+
+    $alumno->nombre = 'Moises';
+    $alumno->apellido_paterno  = 'Lopez';
+    $alumno->apellido_materno  = 'Gil';
+    $alumno->Carrera  = 'DSM';
+    $alumno->save();
+});*/
+//Route::get('alumno', 'alumnos@store');
